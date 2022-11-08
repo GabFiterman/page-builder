fbf9dfd5ca0d17910cddbaf3814322e199776703
GabFiterman <gfiterman96@gmail.com>
2022-11-08 17:21:09 -0300

    Add a structure for mixin Structure for styling componnts Margin-top slider Changes in Paragraph component

--- src/components/elements/Paragraph.vue
+++ src/components/elements/Paragraph.vue
@@ -1,30 +1,66 @@
 <template>
-  <div class="Container">
-    <p>{{ content }}</p>
-  </div>
+  <Editor tag="p" :style="elementCSS" :value="content" @input="updateContent"></Editor>
 </template>
 
 <script>
+import Editor from './utils/Editor.vue';
 import ParagraphSettings from './ParagraphSettings.vue';
+import Margin from '../elements/styleSettings/Margin.vue';
+import elementStyleMixin from '../elements/elementStyleMixin.js';
 
 export default {
-    name: 'Paragraph',
+  name: 'Paragraph',
 
-    props: {
-        content: String,
-    },
+  mixins: [elementStyleMixin],
 
-    craft: {
-        defaultProps: {
-            content: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'
-        },
-        settings: {
-          ParagraphSettings,
-        },
+  props: {
+    content: String,
+  },
+
+  components: {
+    Editor
+  },
+
+  inject: [
+    'node',
+  ],
+
+  methods: {
+    updateContent(content) {
+      this.node.setProps({ content });
     },
+  },
+
+  craft: {
+    defaultProps: {
+      content: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+      elementStyle: {
+      'font-size': 14,
+      'font-weight': 'normal',
+      'text-align': 'left',
+      'line-height': '1.5',
+      color: 'rgb(0,0,0)',
+      'margin-top': 0,
+      'margin-left': 0,
+      'margin-bottom': 0,
+      'margin-right': 0,
+      'padding-top': 0,
+      'padding-left': 0,
+      'padding-bottom': 0,
+      'padding-right': 0,
+      'text-shadow': {
+        x: 0,
+        y: 0,
+        blur: 0,
+        color: 'rgba(0,0,0,0)',
+      },
+    },
+    },
+    settings: {
+      ParagraphSettings,
+      Margin
+    },
+   
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
---
+++ src/components/elements/styleSettings/Margin.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <label class="font-weight-bold" for="topValue">Margin Top: <span class="white text--black px-1 rounded font-weight-bold text-h5">{{getStyle('margin-top')}}</span>px</label>
+        </v-row>
+        <v-row>
+    	    <v-slider
+                :value="getStyle('margin-top')"
+                @input="setStyle('margin-top', $event)"
+                step="2"
+    	        color="white"
+    	        track-color="black"
+    	        thumb-label="true"
+    	        thumb-color="black"
+    	        max="200"
+    	        min="-100"
+    	    ></v-slider>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import mixin from './mixin.js';
+export default {
+    name: 'Margin',
+
+    mixins: [mixin],
+
+    data(){
+        return{
+            topValue: 0,
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
